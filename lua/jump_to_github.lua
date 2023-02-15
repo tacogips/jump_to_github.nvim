b5249f573ae51c104a052f12925f4a6e176741fb
tacogips <me@tacogips.me>
2023-02-15 11:41:08 +0900

    default firefox

--- lua/jump_to_github.lua
+++ lua/jump_to_github.lua
@@ -6,7 +6,7 @@ local buffer = require("jump_to_github.buffer")
 local browser = require("jump_to_github.browser")
 
 local config = {
-	open_browser = "brave",
+	open_browser = "/use/bin/firefox",
 	add_command = true,
 }
 
